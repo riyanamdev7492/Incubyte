3115bba526c14b7d08db6bb91666d4324b27da33
Riya Namdev <riyanamdev2728@gmail.com>
2025-03-15 00:06:48 +0530

    Raise error when negative numbers is passed

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,21 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.gsub("\n", ",").split(',').map(&:to_i).sum
+    result = numbers.gsub("\n", ",").split(',').map(&:to_i)
+
+    check_number(result)
+
+    result.sum
+  end
+
+  private
+
+  def check_number(nums_array)
+    negative_nums = nums_array.select { |num| num < 0 }
+
+    if negative_nums.any?
+      raise "negative numbers not allowed #{negative_nums.join(',')}"
+    end
+
   end
 end
