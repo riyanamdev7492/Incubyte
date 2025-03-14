7a418454f71031c223c7432eafdd633df518772b
Riya Namdev <riyanamdev2728@gmail.com>
2025-03-14 23:38:16 +0530

    Test senerio  for all cases

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,15 +1,35 @@
 require './string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
   describe "#add" do
     it "returns 0 for an empty string" do
-      calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
     end
 
     it "returns number for single number" do
-      calculator = StringCalculator.new
       expect(calculator.add("1")).to eq(1)
+    end
+
+    it "returns the sum of two numbers" do
+      expect(calculator.add("1,5")).to eq(6)
+    end
+
+    it "returns the sum of multiple numbers" do
+      expect(calculator.add("1,2,3,4")).to eq(10)
+    end
+
+    it "returns the sum of numbers seprated with new line" do
+      expect(calculator.add("1\n2, 3, 4")).to eq(10)
+    end
+
+    it "raises an exception for negative numbers" do
+      expect { calculator.add("1,-2") }.to raise_error(RuntimeError, "negative numbers not allowed -2")
+    end
+
+    it "lists all negative numbers in the exception" do
+      expect { calculator.add("1,-2,-3") }.to raise_error(RuntimeError, "negative numbers not allowed -2,-3")
     end
   end
 end
